4cbce620241f7a600637399f006e619ff52c331c
Navdeepuniyal <uniyal.navdeep@gmail.com>
2016-09-27 11:30:03 +0000

    floating_ip -> access_ip_v4

--- terraform/v00/dev/outputs.tf
+++ terraform/v00/dev/outputs.tf
@@ -1,5 +1,5 @@
 output "ipaddr" {
-  value = "${join(",",openstack_compute_instance_v2.sonata-sp.*.floating_ip)}"
+  value = "${join(",",openstack_compute_instance_v2.sonata-sp.*.access_ip_v4)}"
 }
 #output "name" {
 #  value = "${join(",",openstack_compute_instance_v2.sonata-sp.*.name)}"
